507f64cf3a77b3d25cc588ab4b24d6d4265da7c0
elakapmain <notquadrilingual@gmail.com>
2024-10-14 14:51:17 -0500

    Shuffle is working

--- src/views/QuestionPage.vue
+++ src/views/QuestionPage.vue
@@ -25,7 +25,20 @@ onMounted(async () => {
       answer: wrong_answer
     })
   })
+
+  answers.value = shuffle(answers.value)
+  // console.log(question.value)
 })
+
+const shuffle = (array) => {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    // swap (semicolons required)
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+
+  return array
+}
 </script>
 
 <template>
